1de42e52002531f85e3eb0d4e01e697834920fa6
Yash Sharma <yaashg@gmail.com>
2018-05-23 19:55:27 +0530

    prod endpoint

--- intent-trainer/src/store/store.ts
+++ intent-trainer/src/store/store.ts
@@ -44,7 +44,7 @@ export interface Pagination {
 
 let token: any = window.localStorage.getItem("token");
 
-const daptinClient = new DaptinClient("intent-trainer.callup.ai:8080", false);
+const daptinClient = new DaptinClient("http://dashboard.callup.ai:8080", false);
 daptinClient.worldManager.loadModels().then(function () {
 
   if (token) {
